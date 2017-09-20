64630cd1f30620d4ba0a0fe9813b30bc2fb9d973
rahulraval <rahulraval1997@gmail.com>
2017-09-20 23:56:10 +0530

    Completed SellerSchema

---
+++ index.js
@@ -0,0 +1,37 @@
+
+const express= require('express');
+const bodyParser=require('body-parser');
+//const routes=require('./routes/api');
+const mongoose=require('mongoose');
+const port = process.env.PORT || 4000;
+const SellerController=require('./controllers/sellerController');
+const UserController=require('./controllers/userController');
+
+//setting express app
+const app=express();
+
+//connect to databse
+mongoose.connect('mongodb://rahulraval:010297@ds135983.mlab.com:35983/areastore');
+
+//make mongoose global
+mongoose.Promise=global.Promise;
+
+//using bpdy-parser middleware
+app.use(bodyParser.json());
+
+//using SellerController
+app.use('/sellers',SellerController);
+app.use('/',UserController);
+
+//error handling middleware
+app.use(function(err,req,res,next){
+  //console.log(err);
+  res.status(500).send({Error:err.message});
+});
+
+
+//listing request on port
+//if using hiroku then use: process.env.port
+app.listen(port,function(req,res){
+  console.log("Listening at port"+app.get('port')+". . .");
+});
---
+++ models/user.js
@@ -0,0 +1,48 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+//user schema
+const userSchema= new Schema({
+    name:{
+      type:String
+    },
+    email:{
+      type:String
+    },
+    password:{
+      type:String
+    },
+    google:{
+      id:{type:String},
+      token:{type:String}
+    }
+  }
+);
+
+//create module of user and export it to app
+const User=mongoose.model('User',userSchema);
+
+//saving user to db
+module.exports.createUser=function (newUser) {
+    newUser.save();
+}
+
+//getting user by email
+module.exports.getUserByEmail=function (userEmail) {
+    var query={email:userEmail};
+    User.findOne(query);
+}
+//getting user by id
+module.exports.getUserById=function (userId) {
+  User.findById(userId);
+}
+
+/*module.exports.comparePassword=function (password,originalPassword) {
+    if(password.equal(originalPassword)){
+        return true;
+    }
+    else {
+      return false;
+    }
+}*/
+module.exports=User;
